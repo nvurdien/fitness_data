8ea40008d68ed3291743578f442cb023913cdf20
sridattb96 <sridattb.96@gmail.com>
2018-08-16 11:10:31 -0500

    blah

--- fitness_data/src/App.js
+++ fitness_data/src/App.js
@@ -35,10 +35,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">H E A L T H C A R E</h1>
         </header>
         <p className="App-intro">
             <ReactHighcharts config={highchartOptions} />
+
         </p>
       </div>
     );
