feee416fb89b8ba92174225a8132473fa319917f
Navie Vurdien <nvurdien@gmail.com>
2018-08-16 16:03:05 -0500

    2 charts

--- fitness_data/src/App.js
+++ fitness_data/src/App.js
@@ -6,25 +6,55 @@ import ReactHighcharts from'react-highcharts'; // Expects that Highcharts was lo
 
 const highchartOptions = {
     chart: {
-        type: 'bar'
+        type: 'column'
     },
+
     title: {
-        text: 'Calorie Data'
+        text: 'Total fruit consumtion, grouped by gender'
     },
+
     xAxis: {
-        categories: ['Breakfast', 'Lunch', 'Dinner']
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
     },
+
     yAxis: {
+        allowDecimals: false,
+        min: 0,
         title: {
-            text: ''
+            text: 'Number of fruits'
         }
     },
+
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.x + '</b><br/>' +
+                this.series.name + ': ' + this.y + '<br/>' +
+                'Total: ' + this.point.stackTotal;
+        }
+    },
+
+    plotOptions: {
+        column: {
+            stacking: 'normal'
+        }
+    },
+
     series: [{
-        name: 'Jane',
-        data: [1, 0, 4]
-    }, {
         name: 'John',
-        data: [5, 7, 3]
+        data: [5, 3, 4, 7, 2],
+        stack: 'male'
+    }, {
+        name: 'Joe',
+        data: [3, 4, 4, 2, 5],
+        stack: 'male'
+    }, {
+        name: 'Jane',
+        data: [2, 5, 6, 2, 1],
+        stack: 'female'
+    }, {
+        name: 'Janet',
+        data: [3, 0, 4, 4, 3],
+        stack: 'female'
     }]
 };
 
@@ -38,8 +68,15 @@ class App extends Component {
           <h1 className="App-title">H E A L T H C A R E</h1>
         </header>
         <p className="App-intro">
+            <div className='row'>
+            <div className='col-sm-6'>
             <ReactHighcharts config={highchartOptions} />
+            </div>
 
+            <div className='col-sm-6'>
+                <ReactHighcharts config={highchartOptions} />
+            </div>
+            </div>
         </p>
       </div>
     );
